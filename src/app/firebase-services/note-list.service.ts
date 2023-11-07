b50df37ba9df2b94cf6e75359fe34c910ea23043
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-11-07 11:13:51 +0100

    bug with firebase - NullInjectorError

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, doc, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  items$;
+  items;
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() { 
+    this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe( (list) => {
+      list.forEach(element => {
+        console.log(element);
+      });
+    } );
+    this.items.unsubscribe()
+   }
+
+  // const itemCollection = collection(this.firestore, 'items');
+
+
+  getNotesRef() {
+    return collection(this.firestore, 'note');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
+
+
 }
